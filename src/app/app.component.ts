9ca961895ae7ba08e231c55479a074dc89f0b0ca
Sowmya R <sowmyas@baasha.net>
2019-03-26 16:39:10 -0400

    updated the components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  ViewEncapsulation,
+  OnInit,
+  EventEmitter,
+  Output
+} from "@angular/core";
+import { logging } from "protractor";
+import { async } from "q";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  encapsulation: ViewEncapsulation.Native
 })
-export class AppComponent {
-  title = 'login-element';
+export class AppComponent implements OnInit {
+  username: string;
+  password: string;
+  isAuthenticated: boolean;
+
+  title = "login-element";
+
+  @Input()
+  isloggedin: boolean;
+  @Input()
+  logMessage: any;
+  @Output()
+  loginData = new EventEmitter<any>();
+
+  login(uname, pwd) {
+    this.loginData.emit({ user: uname, pass: pwd });
+  }
+  ngOnInit() {
+    this.isAuthenticated = this.isloggedin || false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, Injector } from "@angular/core";
+import { createCustomElement } from "@angular/elements";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const el = createCustomElement(AppComponent, { injector });
+    customElements.define("login-element", el);
+  }
+  ngDoBootstrap() {}
+}
